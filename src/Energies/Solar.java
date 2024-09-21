fe9e0943bc16c740ef9ce0a5e65ded41638252c4
Julian Grisales <julygrisales@gmail.com>
2024-09-21 11:50:18 -0500

    Initial commit

---
+++ src/Energies/Solar.java
@@ -0,0 +1,4 @@
+package Energies;
+
+public class Solar {
+}
---
+++ src/Energies/Wave.java
@@ -0,0 +1,11 @@
+package Energies;
+
+public class Sea extends RenewableEnergy {
+
+    public double powerWave; //KW per meter wave
+
+
+
+
+
+}
